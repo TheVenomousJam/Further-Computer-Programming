a65546d77864725eb89e1b7c652270ed050e9649
ICEWALLOWCOME12 <tc21714@bristol.ac.uk>
2022-04-12 19:37:34 +0100

    Markers now move at same time

--- furprog_planemap.py
+++ furprog_planemap.py
@@ -14,11 +14,11 @@ pos=nx.get_node_attributes(g,'pos')
 FFMpegWriter = manimation.writers['ffmpeg']
 metadata = dict(title='Movie Test', artist='Matplotlib',
                 comment='a red circle following a blue sine wave')
-writer = FFMpegWriter(fps=240, metadata=metadata)
+writer = FFMpegWriter(fps=140, metadata=metadata)
 
 fig = plt.figure()
-populations = [10000,23450,30000,50000,100000]
-popdensity = [5000,2000,8000,10000,6000]
+populations = [10000,23450,30000,50000,100000]#changes size of nodes
+popdensity = [5000,2000,8000,10000,6000]#changes size of nodes
 names = ["A","B","C","D","E"]
 pos=[(1,1),(1,1000),(1000,1000),(1000,1),(500,500)]
 
@@ -30,39 +30,36 @@ for i in range (0,len(populations)):
     labels[names[i]] = f"port {i+1}"     
 
 for i in range (0,5):
-    g.add_node(names[i],pos=pos[i])
+    g.add_node(names[i],pos=pos[i],)
 
 
 g.add_edges_from([("B","C"),("A","B"),("C","D"),("D","E"),("B","E"),("A","E")])
 g.add_edges_from([("D","A"),("C","E")])
 pos=nx.get_node_attributes(g,'pos')
 print(pos)
-nx.draw(g,pos,node_size=areas,labels=labels,with_labels=(True))
-red_circle, = plt.plot([], [], 'ro', markersize = 15, linewidth = 50, label = ("plane"))
+nx.draw(g,pos,node_size=areas,labels=labels,with_labels=(True), node_color='red')
+red_circle1, = plt.plot([], [], 'ro', markersize = 15, linewidth = 50, label = ("plane"))
+red_circle2, = plt.plot([], [], 'ro', markersize = 15, linewidth = 50, label = ("plane"))
+red_circle3, = plt.plot([], [], 'ro', markersize = 15, linewidth = 50, label = ("plane"))
+red_circle4, = plt.plot([], [], 'ro', markersize = 15, linewidth = 50, label = ("plane"))
+red_circle5, = plt.plot([], [], 'ro', markersize = 15, linewidth = 50, label = ("plane"))
 with writer.saving(fig, "writer_test.mp4", 100):
     for i in range(n):
-        red_circle.set_data([1],[i])
-        writer.grab_frame()
-    for i in range(n):
-        red_circle.set_data([i],[1000])
-        writer.grab_frame()
-        
-    for i in range(n):
-        red_circle.set_data([1000],[i])
-        writer.grab_frame()
-    for i in range(n):
-        red_circle.set_data([i],[i])
+        red_circle1.set_data([1],[i])
+        red_circle2.set_data([i],[1000])
+        red_circle3.set_data([1000],[i])
+        red_circle4.set_data([i],[i])
         writer.grab_frame()
     for i in range(n):
         while f > 1:
             f = f-1
-            red_circle.set_data([f],[1])
+            red_circle5.set_data([f],[1])
             writer.grab_frame()
     
 
 
 
-#put all markers moving at once
+
 
     
 
