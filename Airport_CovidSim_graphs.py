cf9f7350bb738ca8f5c7270e6411c5ff4ea719f5
James Jones <gw21491@bristol.ac.uk>
2022-05-03 10:54:34 +0100

    graphs all 5 airports covid journey, can be used as figure in report

--- Airport_CovidSim_graphs.py
+++ Airport_CovidSim_graphs.py
@@ -19,5 +19,26 @@ plt.plot(contagious2, color='orange', label = 'Airport2')
 plt.plot(contagious3, color='yellow', label = 'Airport3')
 plt.plot(contagious4, color='green', label = 'Airport4')
 plt.plot(contagious5, color='blue', label = 'Airport5')
+plt.legend()
 plt.show()
+
+coviddata_AP1 = open('coviddata_AP1.txt','r+')
+coviddata_AP1.truncate(0)
+coviddata_AP1.close()
+
+coviddata_AP2 = open('coviddata_AP2.txt','r+')
+coviddata_AP2.truncate(0)
+coviddata_AP2.close()
+
+coviddata_AP3 = open('coviddata_AP3.txt','r+')
+coviddata_AP3.truncate(0)
+coviddata_AP3.close()
+
+coviddata_AP4 = open('coviddata_AP4.txt','r+')
+coviddata_AP4.truncate(0)
+coviddata_AP4.close()
+
+coviddata_AP5 = open('coviddata_AP5.txt','r+')
+coviddata_AP5.truncate(0)
+coviddata_AP5.close()
 
